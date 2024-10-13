734e28ff16e37c1da0d59ac3188bd160fe593521
Yazan Essam <yazanessam12@gmail.com>
2024-10-13 03:49:00 +0300

    flake test adjust

--- vote/tests/test_app.py
+++ vote/tests/test_app.py
@@ -1,12 +1,13 @@
-import json
 import pytest
 from unittest.mock import patch
 from app import app  # Ensure this path is correct
+
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
 
 @patch('app.Redis')  # Mocking Redis in the vote.app module
 def test_hello_post(mock_redis, client):
@@ -15,18 +16,18 @@ def test_hello_post(mock_redis, client):
     # Simulate successful rpush
 
     response = client.post('/', data={'vote': 'Cats'})
-    
+
     assert response.status_code == 200
     assert b'Cats' in response.data
     # Check that 'Cats' is in the response
     assert b'Dogs' in response.data
     # Check that 'Dogs' is also in the response
-    
+
 
 @patch('app.Redis')
 def test_hello_get(mock_redis, client):
     response = client.get('/')
-    
+
     assert response.status_code == 200
     assert b'Cats' in response.data
     # Check that option_a is in the response
