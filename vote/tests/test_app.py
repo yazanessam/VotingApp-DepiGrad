7fc9407a58d7c0a8feab4f5e57877687dc6ac373
Yazan Essam <yazanessam12@gmail.com>
2024-10-13 03:01:33 +0300

    edit flask test files #6

--- vote/tests/test_app.py
+++ vote/tests/test_app.py
@@ -1,27 +1,18 @@
-# tests/test_app.py
-
 import pytest
-from vote.app import app  # Ensure to import the app correctly
-
+from vote.app import app  # Ensure this path is correct
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
-
-def test_home_page(client):
-    """Test loading the home page"""
+def test_hello_get(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'Cats' in response.data
     assert b'Dogs' in response.data
 
-
-def test_post_vote(client):
-    """Test submitting a vote"""
+def test_hello_post(client):
     response = client.post('/', data={'vote': 'Cats'})
     assert response.status_code == 200
-    assert (
-        b'Received vote for Cats' in response.data
-    )  # Check that the message about receiving the vote is present
+    assert b'Received vote for Cats' in response.data
