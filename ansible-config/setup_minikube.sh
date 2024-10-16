1c8a0fc80fc586dfd8034f08d0c7e6cb5e4c9760
Yazan Essam <yazanessam12@gmail.com>
2024-10-16 16:52:46 +0000

    edit setup_minikube script #3

--- ansible-config/setup_minikube.sh
+++ ansible-config/setup_minikube.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo systemctl start docker
-sudo systemctl enable docker
+systemctl start docker
+systemctl enable docker
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https curl
+apt-get update
+apt-get install -y apt-transport-https curl
 
 if ! command -v docker &> /dev/null; then
-    sudo apt-get install -y docker.io
+    apt-get install -y docker.io
 fi
 
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube /usr/local/bin/
+install minikube /usr/local/bin/
 
 minikube start --driver=docker
 
