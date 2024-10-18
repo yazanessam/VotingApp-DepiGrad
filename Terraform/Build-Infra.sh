0ac6af3dd0dcfeaf21d2308332f455891debcd52
Yazan Essam <yazanessam12@gmail.com>
2024-10-18 04:13:26 +0300

    edit build-infra script

--- Terraform/Build-Infra.sh
+++ Terraform/Build-Infra.sh
@@ -4,4 +4,4 @@ export TF_VAR_vm_ssh_key=$CI_VM_SSH_PUBLIC_KEY
 echo "Initializing Terraform..."
 terraform init
 echo "Applying Terraform configuration..."
-terraform apply -auto-approve | tee terraform-output.txt
+terraform apply -auto-approve -var="admin_username=$CI_TF_USERNAME" | tee terraform-output.txt
