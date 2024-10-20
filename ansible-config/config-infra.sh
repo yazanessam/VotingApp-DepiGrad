b49c8ac6cab11c734010ff094043c84cec115cdb
Yazan Essam <yazanessam12@gmail.com>
2024-10-20 19:26:05 +0300

    edit minikube.yaml

--- ansible-config/config-infra.sh
+++ ansible-config/config-infra.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 chmod 400 gitlab_rsa
-python ./set-inventory.py
+python3 --version
+python3 ./set-inventory.py
 ansible-galaxy collection install community.general
 ansible-playbook -i inventory.ini minikube.yml | tee .minikube-playbook-output.txt
 
