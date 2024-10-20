353dfa2556f0e9c763ac5e4f1b7843255712396e
Yazan Essam <yazanessam12@gmail.com>
2024-10-21 02:20:03 +0300

    add python update to minikube playbook

--- Terraform/Build-Infra.sh
+++ Terraform/Build-Infra.sh
@@ -8,5 +8,6 @@ terraform init
 echo "Applying Terraform configuration..."
 terraform apply -auto-approve \
     -var="admin_username=$TF_USERNAME" \
-    -var="subscription_id=$TF_AZURE_SUB_ID" | tee terraform-output.txt
+    -var="subscription_id=$TF_AZURE_SUB_ID" \
+    -var="admin_ssh_key=$TF_VAR_vm_ssh_key"| tee terraform-output.txt
 terraform output -json > terraform-output.json
