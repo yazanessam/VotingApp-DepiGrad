655c5257ad27b7f2d1f98f2f0857453841cf0f1e
Yazan Essam <yazanessam12@gmail.com>
2024-10-21 06:31:38 +0300

    edit image version for minikube machine

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -102,7 +102,7 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "22.04-LTS"
     version   = "latest"
   }
 
