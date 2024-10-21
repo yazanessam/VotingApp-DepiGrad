0c40a6054339a42b0e88880edd46e2cdb6ef9ee3
Yazan Essam <yazanessam12@gmail.com>
2024-10-21 15:19:59 +0300

    edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sh ./k8s-specifications/build-deployments/build-result-deployment.sh
-sh ./k8s-specifications/build-deployments/build-vote-deployment.sh
-sh ./k8s-specifications/build-deployments/build-worker-deployment.sh
-kubectl apply -f ./k8s-specifications/build-deployments/namespaces.yaml
-kubectl apply -f ./k8s-specifications/build-deployments/result-deployment.yml -n "$CI_ENV"
-kubectl apply -f ./k8s-specifications/build-deployments/vote-deployment.yml -n "$CI_ENV"
-kubectl apply -f ./k8s-specifications/build-deployments/worker-deployment.yml -n "$CI_ENV"
+sh ./build-result-deployment.sh
+sh ./build-vote-deployment.sh
+sh ./build-worker-deployment.sh
+kubectl apply -f ./namespaces.yaml
+kubectl apply -f ./result-deployment.yml -n "$CI_ENV"
+kubectl apply -f ./vote-deployment.yml -n "$CI_ENV"
+kubectl apply -f ./worker-deployment.yml -n "$CI_ENV"
