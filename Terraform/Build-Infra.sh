b2a9544ab00c5d9446df9e7ff192555367c379a2
Yazan Essam <yazanessam12@gmail.com>
2024-10-21 05:08:06 +0300

    Edit build-infra script #3

--- Terraform/Build-Infra.sh
+++ Terraform/Build-Infra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export TF_VAR_vm_ssh_key=$CI_VM_SSH_PUBLIC_KEY
-export TF_VAR_PRIVATE_vm_ssh_key=CI_VM_SSH_PRIVATE_KEY
+export TF_VAR_PRIVATE_vm_ssh_key=$CI_VM_SSH_PRIVATE_KEY
 export TF_USERNAME=$CI_TF_USERNAME
 export TF_AZURE_SUB_ID=$CI_ARM_SUBSCRIPTION_ID
 echo "Initializing Terraform..."
